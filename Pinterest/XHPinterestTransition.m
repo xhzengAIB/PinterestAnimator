cc0f2bcab7000376919c71d3317741698431a029
Jack <xhzengAIB@gmail.com>
2014-07-14 11:46:54 +0800

    添加基础文件

---
+++ Pinterest/XHPinterestTransition.m
@@ -0,0 +1,13 @@
+//
+//  XHPinterestTransition.m
+//  PinterestExample
+//
+//  Created by dw_iOS on 14-7-14.
+//  Copyright (c) 2014年 广州华多网络科技有限公司 多玩事业部 iOS软件工程师 曾宪华. All rights reserved.
+//
+
+#import "XHPinterestTransition.h"
+
+@implementation XHPinterestTransition
+
+@end
