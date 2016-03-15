57c4feaa3dfe6e3ce7dc7c4821df431c444535df
Jack <xhzengAIB@gmail.com>
2016-03-15 13:58:14 +0800

    修改描述

--- PinterestAnimator/Helpers/NavigationHelper/Transition/XHPinterestTransition.m
+++ PinterestAnimator/Helpers/NavigationHelper/Transition/XHPinterestTransition.m
@@ -3,7 +3,7 @@
 //  PinterestExample
 //
 //  Created by dw_iOS on 14-7-14.
-//  Copyright (c) 2014年 广州华多网络科技有限公司 多玩事业部 iOS软件工程师 曾宪华. All rights reserved.
+//  Copyright (c) 2014年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
 
 #import "XHPinterestTransition.h"
